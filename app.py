7e07d8d8b4b5f20b07be346a05a7c811957d3f48
SapienData <mohan.gopal@sapiendata.com.au>
2025-08-13 16:29:42 +1000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import plotly.express as px
 # ---------------------------
 st.set_page_config(page_title="Beauty Business Dashboard", layout="wide")
 
-st.image("Sapiedata.png", width=120)
+st.image("Sapiendata.png", width=120)
 st.markdown("""
     <h1 style='color:#d63384; font-weight:bold; margin-top: -20px;'>Sapien Data</h1>
 """, unsafe_allow_html=True)
